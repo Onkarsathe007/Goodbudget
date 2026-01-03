ddde4872da26f47826e555fd47af854a018158b8
Onkar <onkarsathe96k@gmail.com>
2026-01-03 13:35:27 +0530

    feat(logs): Add log monitoring configuration
    Implement initial configuration for application logging and monitoring.
    Introduce src/config/logs.config.ts to centralize log-related settings.
    Update package.json and pnpm-lock.yaml to support new logging dependencies.
    Modify .gitignore to prevent tracking of new log outputs.

---
+++ src/config/logs.config.ts
@@ -0,0 +1,37 @@
+import winston from "winston";
+const { combine, timestamp, printf, colorize, errors } = winston.format;
+
+const logFormat = printf(({ level, message, timestamp, stack }) => {
+  return `${timestamp} [${level}]: ${stack || message}`;
+});
+
+const logger = winston.createLogger({
+  level: "info",
+
+  format: combine(
+    errors({ stack: true }),
+    timestamp({ format: "YYYY-MM-DD HH:mm:ss" }),
+    logFormat,
+  ),
+  defaultMeta: { service: "goodbudget-api-0.0.0" },
+  transports: [
+    new winston.transports.Console({
+      format: combine(colorize(), logFormat),
+    }),
+
+    // - Write all logs with importance level of `error` or higher to `error.log`
+    //   (i.e., error, fatal, but not other levels)
+
+    new winston.transports.File({
+      filename: "./logs/error.log",
+      level: "error",
+    }),
+
+    // - Write all logs with importance level of `info` or higher to `combined.log`
+    //   (i.e., fatal, error, warn, and info, but not trace)
+
+    new winston.transports.File({ filename: "./logs/combined.log" }),
+  ],
+});
+
+export default logger;
