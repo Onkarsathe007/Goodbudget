7d1e68a2b557a390760e9c800d1a760fa48d323b
Onkar <onkarsathe96k@gmail.com>
2026-01-10 15:53:07 +0530

    chore(expenses): update routes file

--- src/routes/expenses.routes.ts
+++ src/routes/expenses.routes.ts
@@ -1,11 +1,20 @@
 import express, { type Router } from "express";
 import expenseController from "../controllers/expense.controller.js";
+import { requireAdmin, requireAuth } from "../middlewares/auth.middleware.js";
 
 const expenseRouter: Router = express.Router();
 
-expenseRouter.get("/expense", expenseController.getExpenses);
-expenseRouter.get("/expense/:id", expenseController.getExpenseById);
-expenseRouter.post("/expense", expenseController.setExpenses);
-expenseRouter.put("/expense/:id", expenseController.updateExpense);
-expenseRouter.delete("/expense/:id", expenseController.deleteExpense);
+expenseRouter.get("/expense", requireAuth, expenseController.getExpenses);
+expenseRouter.get(
+  "/expense/:id",
+  requireAuth,
+  expenseController.getExpenseById,
+);
+expenseRouter.post("/expense", requireAuth, expenseController.setExpenses);
+expenseRouter.put("/expense/:id", requireAuth, expenseController.updateExpense);
+expenseRouter.delete(
+  "/expense/:id",
+  requireAuth,
+  expenseController.deleteExpense,
+);
 export default expenseRouter;
