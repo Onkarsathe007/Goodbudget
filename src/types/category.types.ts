2f76bceac5c3638bf98344b1736f9d07bdfcc562
Onkar <onkarsathe96k@gmail.com>
2026-01-19 16:24:51 +0530

    fix(validation): remove invalid accountId from category schema and make accountId required in expense schema

--- src/types/category.types.ts
+++ src/types/category.types.ts
@@ -4,6 +4,5 @@ export const categorySchema = z.object({
   id: z.number().nonnegative().optional(),
   name: z.string(),
   type: z.enum(["EXPENSE", "INCOME"]),
-  accountId: z.string(),
   isDefault: z.boolean().default(false),
 });
--- src/types/expenses.types.ts
+++ src/types/expenses.types.ts
@@ -3,7 +3,7 @@ import z from "zod";
 export const expenseSchema = z.object({
   id: z.string().optional(),
   userId: z.string(),
-  accountId: z.string().optional(),
+  accountId: z.string(),
   categoryId: z.number().nonnegative(),
   title: z.string().nullable(),
   note: z.string().nullable(),
